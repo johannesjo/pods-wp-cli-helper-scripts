32487b2ebf30a1e0a92065d608e7d5435a9c6cc0
johannesjo <johannes.millan@gmail.com>
2013-10-02 21:17:43 +0200

    trytrytry

--- pods.py
+++ pods.py
@@ -1,7 +1,41 @@
-podName = raw_input("name of pod to set: ")
-podType = raw_input("type of [" + podName + "] to set: ")
+# import subprocess
+
+sPodName = raw_input("name of pod to add fields to: ")
+
+aFields = []
+aValues = []
 
 
+def fieldsToAdd():
+    global aFields
 
-print podName
-print podType
+    sFieldName = raw_input("field-name (empty for continue): ")
+    print(sFieldName)
+
+    if sFieldName == '':
+        valuesToAdd()
+    else:
+        aFields.append(sFieldName)
+        fieldsToAdd()
+
+
+def valuesToAdd():
+    global aValues, aFields
+    for (i, item) in enumerate(aFields):
+        sValueName = raw_input(item + " -Value: ")
+        aValues.append(sValueName)
+
+    output()
+
+
+def output():
+    global aFields, aValues, sPodName
+    raw_input("everything alright?? CTRL C to cancel")
+    print(aFields, aValues, sPodName)
+
+    # notifySend = ['notify-send', title, msg]
+    # process = subprocess.Popen(notifySend, stdout=subprocess.PIPE)
+    # output = process.communicate()[0]
+
+
+fieldsToAdd()
